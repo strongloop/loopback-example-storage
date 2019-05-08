82bff1ba897851965fa1718cf9a99e9e6dd9496c
Agnes Lin <agneslin.lin@ibm.com>
2019-05-08 09:29:05 -0400

    squashed commits chore: update copyrights years

--- example/app-cloud.js
+++ example/app-cloud.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-example-storage
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 var StorageService = require('../').StorageService;
 var path = require('path');
 var providers = null;
